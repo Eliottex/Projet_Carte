bd2bf9f9d3b8b64355e679842ef3485e000a4ac7
Eliottex <franchomme.eliott@gmail.com>
2025-04-24 11:00:46 +0200

    Add files via upload

--- MapMaker/MapMaker_v2.py
+++ MapMaker/MapMaker_v2.py
@@ -13,40 +13,12 @@ larg_carreau=int(largeur/nbr_carreau)
 
 liste_tuiles = os.listdir( 'tuiles/' )
 
-def prepare_lst_tuiles(liste_tuiles_possibles):
-     
-    longueur=800
-    largeur=800
-    nbr_carreau=10
-    long_carreau=int(longueur/nbr_carreau)
-    larg_carreau=int(largeur/nbr_carreau)
-    #print('salut')
-
-
-
-
-    fltk.efface_tout()
-
-    num_lig=1
-    num_col=0
-    tableau_possibles= [[[] for x in range(nbr_carreau+1)] for x in range(len(liste_tuiles_possibles)//nbr_carreau+1)]
-    #print(tableau_possibles)
-    for x in range(len(liste_tuiles_possibles)):
-        #print( num_lig,num_col,len(liste_tuiles_possibles)//nbr_carreau+1,tableau_possibles)
-        #fltk.image(num_lig*larg_carreau , num_col*long_carreau , "tuiles/"+liste_tuiles_possibles[x], long_carreau, larg_carreau, ancrage='nw', tag='image'+str(num_lig)+str(num_col))   
-        tableau_possibles[num_col][num_lig-1] = liste_tuiles_possibles[x]
-        if num_lig%(nbr_carreau+1)==0 and x!=0:
-            num_col+=1
-            num_lig=1
-        else:
-            num_lig +=1
-    liste_num_affichage=[[(y,x) for x in range(nbr_carreau)] for y in range(nbr_carreau)]
-    #print(liste_num_affichage)
-    return liste_num_affichage
-
-    #print(tableau_possibles)
-    
 def afficher_tuiles(liste_num,liste_tuiles,larg_car,long_car,nbr_car):
+    """Affiche les tuiles possibles pour la case cliquée.
+    
+
+    """
+    
     fltk.efface_tout()
     #print(liste_tuiles,len(liste_tuiles))
     for x in range(len(liste_num)):
@@ -58,12 +30,25 @@ def afficher_tuiles(liste_num,liste_tuiles,larg_car,long_car,nbr_car):
 
     quad(nbr_carreau,longueur,largeur,long_carreau,larg_carreau)
 
+
 def quad(nbr_carreaux,long_fenetre,larg_fenetre,long_car,larg_car):
+    """Fait un quadrillage.
+
+    
+    """
+    
     for x in range(1,nbr_carreaux):
         fltk.ligne((x)*larg_car,long_fenetre,x*larg_car,0)
         fltk.ligne(larg_fenetre,(x)*long_car,0,x*long_car)
 
+
+
 def afficher_carte(nbr_car,long,larg,long_car,larg_car):
+    """Affiche la carte actuelle.
+    
+    
+    """
+    
     fltk.efface_tout()
     quad(nbr_car,long,larg,long_car,larg_car)
     for x in range(len(list_image)):
@@ -74,22 +59,25 @@ def afficher_carte(nbr_car,long,larg,long_car,larg_car):
 
 
 
-
+#liste dans laquelle sera retenue l'occupation de chaque case
 list_image=[['Vide' for x in range(nbr_carreau)] for y in range(nbr_carreau)]
-quad(nbr_carreau,longueur,largeur,long_carreau,larg_carreau)
 
+
+#création d'un mode permettant d'alterner entre le choix de la case et le choix de la tuile pour la case
 mode='choisir_case'
+
+quad(nbr_carreau,longueur,largeur,long_carreau,larg_carreau)
 
 while True:
     ev = fltk.donne_ev()
     tev = fltk.type_ev(ev)
-    if ev!=None :
-        print(ev,tev)
 
     if tev== "Scroll":
         if mode == 'choisir_tuile':
+
             direction = int(fltk.attribut(ev,"delta")/120)
-            #print(liste_num_affichage)
+
+            #scroll vers le haut en déalant les images
             if liste_num_affichage[0][0][0]!=0 and direction == 1:
                 for x in range(len(liste_num_affichage)):
                     for y in range(len(liste_num_affichage[x])):
@@ -97,21 +85,25 @@ while True:
                         col = liste_num_affichage[x][y][0]
                         liste_num_affichage[x][y]=(col-direction,lig)
                         
+            #scroll vers le bas en décalant les images            
             if liste_num_affichage[-1][-1][0]<(len(list_valide)//nbr_carreau) and direction == -1:
                 for x in range(len(liste_num_affichage)):
                     for y in range(len(liste_num_affichage[x])):
                         lig = liste_num_affichage[x][y][1]
                         col = liste_num_affichage[x][y][0]
                         liste_num_affichage[x][y]=(col-direction,lig)
-            #print(liste_num_affichage)
+
+            #affiche la nouvelle liste d'image (avec décalage du scroll)
             afficher_tuiles(liste_num_affichage,list_valide,larg_carreau,long_carreau,nbr_carreau)
 
     
     if tev == "ClicGauche":
         if mode == 'choisir_case':
+            #Récupère les coordonnées de la case cliquée
             num_col=fltk.ordonnee_souris()//long_carreau
             num_lig=fltk.abscisse_souris()//larg_carreau
-            print(num_col,num_lig)
+
+            #vérifie si il y a une case sur les côtés, si oui elle retiens le raccord nécessaire
             if num_lig-1 >=0:
                 gauche = list_image[num_col][num_lig-1][1] if list_image[num_col][num_lig-1]!='Vide' else '0' 
             else :
@@ -129,31 +121,43 @@ while True:
             else :
                 bas = '0'
 
-            #print('haut',haut,'droite',droite,'bas',bas,'gauche',gauche)
-
+            #Recherche l'ensemble des cases qui correspondent aux critères demandés
             list_valide=[]
             for tuile in liste_tuiles:
                 if (tuile[3]==gauche or gauche=='0') and (tuile[1]==droite or droite=='0') and (tuile[0]==haut or haut=='0') and (tuile[2]==bas or bas=='0'):
                     list_valide.append(tuile)
-            print(list_valide,len(list_valide))
-            
 
+            
+            #Si il y a au moins une case valide, affiche tous les tuiles possibles à l'aide de la fonction afficher_tuiles
             if list_valide!=[]:
-                liste_num_affichage = prepare_lst_tuiles(list_valide)
+                liste_num_affichage=[[(y,x) for x in range(nbr_carreau)] for y in range(nbr_carreau)]
                 afficher_tuiles(liste_num_affichage,list_valide,larg_carreau,long_carreau,nbr_carreau)
+                #Puis modifie le mode pour passer en choix de la tuile
                 mode='choisir_tuile'
 
 
         elif mode=='choisir_tuile':
-            print(num_col,num_lig)
+            
+            #Récupère les coordonées de la tuile cliquée
             x=fltk.ordonnee_souris()//long_carreau
             y=fltk.abscisse_souris()//larg_carreau
+
+            #Si le clique est bien sur une tuile (et pas une image vide):
             if liste_num_affichage[x][y][1]+nbr_carreau*liste_num_affichage[x][y][0]<len(list_valide):
+
+                #Alors on définit la nouvelle case
+                #Ici on utilise x et y qui correspondent à l'emplacement de la tuile choisie
                 case=list_valide[liste_num_affichage[x][y][1]+nbr_carreau*liste_num_affichage[x][y][0]]
-                print(liste_num_affichage[x][y][1]+nbr_carreau*liste_num_affichage[x][y][0])
-                list_image[num_col][num_lig]=case
-                #print(num_lig*larg_carreau , num_col*long_carreau)
+
+
+                #On l'ajoute à la liste                                
+                #ici on utilise num_col et num_lig et pas x et y car on a besoin des coordonées de la case cliquée avant et non ceux de la tuile choisie
+                list_image[num_col][num_lig]=case    
+
+                #Et on réaffiche toutes les cases avec la liste modifiée juste avant
                 afficher_carte(nbr_carreau,longueur,largeur,long_carreau,larg_carreau)
+
+                #On rechange le mode pour choisir une nouvelle case
                 mode = 'choisir_case'
 
 
@@ -161,6 +165,7 @@ while True:
 
     if tev == "ClicDroit":
         if mode == 'choisir_case':
+            #Récupère les coordonées de la case à supprimer
             y_sup=fltk.ordonnee_souris()//long_carreau
             x_sup=fltk.abscisse_souris()//larg_carreau
             #Efface l'image graphiquement
@@ -173,10 +178,12 @@ while True:
         touche_ev = fltk.touche(ev)
         
         if touche_ev == "l":
+
+            
             num_col=fltk.ordonnee_souris()//long_carreau
             num_lig=fltk.abscisse_souris()//larg_carreau
-            #print(fltk.objet_survole())
-            #print(list_image[num_col][num_lig])
+            
+            print(list_image[num_col][num_lig])
 
     if tev == "Quitte":
 
