b09d8baaa380d271b22226bc4b8a147d239e4ead
rougevif <linakhadhri05@gmail.com>
2025-04-29 10:22:27 +0200

    affichage du menu des tuiles

--- MapMaker/MapMaker_v2.py
+++ MapMaker/MapMaker_v2.py
@@ -5,15 +5,18 @@ import random
 
 hauteur=800 
 largeur=800
+decalage_fenetre=200
 fltk.cree_fenetre(largeur,hauteur)
 nbr_carreau=10
-long_carreau=int(hauteur/nbr_carreau)
+haut_carreau=int(hauteur/nbr_carreau)
 larg_carreau=int(largeur/nbr_carreau)                                                                
+decalage_haut=haut_carreau -20
+decalage_larg=larg_carreau -20   
 
 
 liste_tuiles = os.listdir( 'tuiles/' )
 
-def afficher_tuiles(liste_num,liste_tuiles,larg_car,long_car,nbr_car):
+def afficher_tuiles(liste_num,liste_tuiles,larg_car,haut_car,nbr_car):
     """Affiche les tuiles possibles pour la case cliquée.
     
 
@@ -21,25 +24,27 @@ def afficher_tuiles(liste_num,liste_tuiles,larg_car,long_car,nbr_car):
     
     fltk.efface_tout()
     #print(liste_tuiles,len(liste_tuiles))
+    fltk.rectangle(100,100,700,700,"pink","pink")
     for x in range(len(liste_num)):
         for y in range(len(liste_num[x])):
             num_ligne = liste_num[x][y][1]
             num_colon = liste_num[x][y][0]
+            
             if 10*num_colon+num_ligne < len(liste_tuiles):
-                fltk.image(y*larg_car , x*long_car , "tuiles/"+liste_tuiles[nbr_car*num_colon+num_ligne], long_car, larg_car, ancrage='nw', tag='image'+str(num_ligne)+str(num_colon))   
+                fltk.image(y*decalage_larg+105 , x*decalage_haut+105, "tuiles/"+liste_tuiles[nbr_car*num_colon+num_ligne],decalage_haut-10, decalage_larg-10, ancrage='nw', tag='image'+str(num_ligne)+str(num_colon))   
 
-    quad(nbr_carreau,hauteur,largeur,long_carreau,larg_carreau)
+    quad(nbr_carreau,hauteur-decalage_fenetre,largeur-decalage_fenetre,decalage_haut,decalage_larg,"pink",100)
 
 
-def quad(nbr_carreaux,long_fenetre,larg_fenetre,long_car,larg_car):
-    """Fait un quadrillage.
+def quad(nbr_carreaux,long_fenetre,larg_fenetre,long_car,larg_car,couleur="black",decal=0):
+    """Fait un quadrillage.fltk.rectangle(100,100,700,700,"pink","white",epaisseur="5")
 
     
     """
     
     for x in range(1,nbr_carreaux):
-        fltk.ligne((x)*larg_car,long_fenetre,x*larg_car,0)
-        fltk.ligne(larg_fenetre,(x)*long_car,0,x*long_car)
+        fltk.ligne((x)*larg_car+decal,long_fenetre+decal,x*larg_car+decal,decal,couleur)
+        fltk.ligne(larg_fenetre+decal,(x)*long_car+decal,decal,x*long_car+decal,couleur)
 
 
 
@@ -66,7 +71,7 @@ list_image=[[None for x in range(nbr_carreau)] for y in range(nbr_carreau)]
 #création d'un mode permettant d'alterner entre le choix de la case et le choix de la tuile pour la case
 mode='choisir_case'
 
-quad(nbr_carreau,hauteur,largeur,long_carreau,larg_carreau)
+quad(nbr_carreau,hauteur,largeur,haut_carreau,larg_carreau)
 
 while True:
     ev = fltk.donne_ev()
@@ -97,13 +102,13 @@ while True:
                         liste_num_affichage[x][y]=(col-direction,lig)
 
             #affiche la nouvelle liste d'image (avec décalage du scroll)
-            afficher_tuiles(liste_num_affichage,list_valide,larg_carreau,long_carreau,nbr_carreau)
+            afficher_tuiles(liste_num_affichage,list_valide,larg_carreau,haut_carreau,nbr_carreau)
 
     
     if tev == "ClicGauche":
         if mode == 'choisir_case':
             #Récupère les coordonnées de la case cliquée
-            num_col=fltk.ordonnee_souris()//long_carreau
+            num_col=fltk.ordonnee_souris()//haut_carreau
             num_lig=fltk.abscisse_souris()//larg_carreau
 
             #vérifie si il y a une case sur les côtés, si oui elle retiens le raccord nécessaire
@@ -134,7 +139,7 @@ while True:
             #Si il y a au moins une case valide, affiche tous les tuiles possibles à l'aide de la fonction afficher_tuiles
             if list_valide!=[]:
                 liste_num_affichage=[[(y,x) for x in range(nbr_carreau)] for y in range(nbr_carreau)]
-                afficher_tuiles(liste_num_affichage,list_valide,larg_carreau,long_carreau,nbr_carreau)
+                afficher_tuiles(liste_num_affichage,list_valide,larg_carreau,haut_carreau,nbr_carreau)
                 #Puis modifie le mode pour passer en choix de la tuile
                 mode='choisir_tuile'
 
@@ -142,7 +147,7 @@ while True:
         elif mode=='choisir_tuile':
             
             #Récupère les coordonées de la tuile cliquée
-            x=fltk.ordonnee_souris()//long_carreau
+            x=fltk.ordonnee_souris()//haut_carreau
             y=fltk.abscisse_souris()//larg_carreau
 
             #Si le clique est bien sur une tuile (et pas une image vide):
@@ -158,7 +163,7 @@ while True:
                 list_image[num_col][num_lig]=case    
 
                 #Et on réaffiche toutes les cases avec la liste modifiée juste avant
-                afficher_carte(nbr_carreau,hauteur,largeur,long_carreau,larg_carreau)
+                afficher_carte(nbr_carreau,hauteur,largeur,haut_carreau,larg_carreau)
 
                 #On rechange le mode pour choisir une nouvelle case
                 mode = 'choisir_case'
@@ -169,7 +174,7 @@ while True:
     if tev == "ClicDroit":
         if mode == 'choisir_case':
             #Récupère les coordonées de la case à supprimer
-            y_sup=fltk.ordonnee_souris()//long_carreau
+            y_sup=fltk.ordonnee_souris()//haut_carreau
             x_sup=fltk.abscisse_souris()//larg_carreau
             #Efface l'image graphiquement
             fltk.efface('image'+str(x_sup)+str(y_sup))
@@ -183,7 +188,7 @@ while True:
         if touche_ev == "l":
 
             
-            num_col=fltk.ordonnee_souris()//long_carreau
+            num_col=fltk.ordonnee_souris()//haut_carreau
             num_lig=fltk.abscisse_souris()//larg_carreau
             
             print(list_image[num_col][num_lig])
